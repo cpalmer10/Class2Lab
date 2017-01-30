71d739ab71e22bcdd8ed6e25cc6110510fafc775
cpalmer10 <cpalmer10@my.wctc.edu>
2017-01-30 11:14:00 -0600

    second commit

---
+++ src/java/model/WelcomeService.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class WelcomeService {
+    final int timeUntilAfternoon = 12;
+    final int timeUntilEvening = 19;
+    
+    Calendar cal = Calendar.getInstance();
+    
+    String partOfDay;
+    String greeting;
+    
+    int hour = cal.get(Calendar.HOUR_OF_DAY);
+    
+    
+    public void DetermineTimeOfDay(){
+        //Morning
+        if(hour < 12) {
+            partOfDay = "morning";
+        }
+        else if (hour > 12 && hour < 17){
+            partOfDay = "afternoon";
+        }
+        else {
+            partOfDay = "evening";
+        }
+            
+        //Afternoon
+        
+        //Evening
+    }
+    
+    public void greeting(){
+        greeting = "Good" + partOfDay + ", " + name + ". Welcome!";
+}
+    
+    
+}
---
+++ web/Welcome.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : Welcome
+    Created on : Jan 30, 2017, 11:02:09 AM
+    Author     : Chris
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Greeting Form</title>
+    </head>
+    <body>
+        <h1>Greeting</h1>
+        <h2>Please enter your name:</h2>
+        Name:<input type="text" name="myName" value=""/><br>
+            <input type="submit" name="submit" value="submit"/>
+    </body>
+</html>
